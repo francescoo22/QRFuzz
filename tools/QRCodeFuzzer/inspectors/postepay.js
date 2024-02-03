36345f4e89d10085eba1a39f3f3c8212dae326aa
francesco <francesco.protopapa22@gmail.com>
2024-02-03 16:54:30 +0100

    Update postepay inspector

--- tools/QRCodeFuzzer/inspectors/postepay.js
+++ tools/QRCodeFuzzer/inspectors/postepay.js
@@ -4,11 +4,15 @@ class Inspector {
     app_activity = "posteitaliane.posteapp.apppostepay.ui.activity.SplashActivity";
 
     async goToScan(driver) {
-    	let ok = await driver.findElement("id", 'posteitaliane.posteapp.apppostepay:id/md_buttonDefaultPositive');
+    	let ok = await driver.findElement("id", 'posteitaliane.posteapp.apppostepay:id/second_bt');
         await driver.elementClick(ok.ELEMENT);
+
+        await driver.pause(1000);
+
+        // TODO: instructions for the page that appears only the first time
         
     	let scan = await driver.findElement("id", 'posteitaliane.posteapp.apppostepay:id/access_qr');
-        await driver.elementClick(scan.ELEMENT);       
+        await driver.elementClick(scan.ELEMENT);
        
     }
 
@@ -20,9 +24,16 @@ class Inspector {
     	let ok = await driver.findElement("id", 'posteitaliane.posteapp.apppostepay:id/md_buttonDefaultPositive');
         await driver.elementClick(ok.ELEMENT);
 
+        await driver.pause(1000);
+
+        let ok2 = await driver.findElement("id", 'posteitaliane.posteapp.apppostepay:id/second_bt');
+        await driver.elementClick(ok2.ELEMENT);
+
+        await driver.pause(1000);
+
     	let scan = await driver.findElement("id", 'posteitaliane.posteapp.apppostepay:id/access_qr');
-        await driver.elementClick(scan.ELEMENT);   
-        }
+        await driver.elementClick(scan.ELEMENT);
+    }
 
 }
 
